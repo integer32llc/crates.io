8a6c74325bb793dfd55a7c0bee3a88eae23f21f8
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2017-01-29 17:55:29 -0500

    Display the latest version and targets this crate version builds on
    Icons from https://github.com/vorillaz/devicons

--- src/version.rs
+++ src/version.rs
@@ -71,6 +71,7 @@ pub struct VersionLinks {
     pub dependencies: String,
     pub version_downloads: String,
     pub authors: String,
+    pub build_info: String,
 }
 
 #[derive(Insertable, AsChangeset)]
@@ -205,6 +206,7 @@ impl Version {
                 version_downloads: format!("/api/v1/crates/{}/{}/downloads",
                                            crate_name, num),
                 authors: format!("/api/v1/crates/{}/{}/authors", crate_name, num),
+                build_info: format!("/api/v1/crates/{}/{}/build_info", crate_name, num),
             },
         }
     }
